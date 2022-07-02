502d20be154cd1d1131196db5ade6491b4abb942
MurilloAssis <murillo98asiss@gmail.com>
2022-07-02 09:58:17 -0300

    CHORE: Tela login consumindo API, falta estilização

--- FrontEnd/2rp/src/Pages/Login/Login.jsx
+++ FrontEnd/2rp/src/Pages/Login/Login.jsx
@@ -1,9 +1,39 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import api from '../../services/api'
+import { useHistory } from 'react-router-dom'
 
 
 export default function Login(){
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('')
+
+    const FazerLogin = (event) => {
+        event.preventDefault();
+
+        api.post('/Login',{
+            email: email,
+            senha: senha
+        })
+        .then(resposta => {
+            if(resposta.status === 200){
+                localStorage.setItem('usuario-token', resposta.data.token)
+            }
+        })
+        .catch(resposta => {
+            console.log(resposta)
+        })
+    }
+
     return(
         <div>
-            <p>Login</p>
+            <form onSubmit={(event) => FazerLogin(event)}>
+                <input type="email" name="email" placeholder="Digite seu email" valu={email} onChange={(evt) => setEmail(evt.target.value)}/>
+                <label for="email">Email</label>
+                <input type="password" name="senha" placeholder="Digite sua senha" valu={senha} onChange={(evt) => setSenha(evt.target.value)}/>
+                <label for="senha">Senha</label>
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
